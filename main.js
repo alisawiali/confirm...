1db43e153662cf2f24bba7e347c5a27ad47d157e
alisawiali <alisawiali70@gmail.com>
2023-03-10 17:18:44 +0100

    apdate scroll

--- main.js
+++ main.js
@@ -15,11 +15,12 @@ const mybtn = document.querySelector("button");
 console.log(mybtn)
 
 
-const myConfirom = confirm( "Do you want to open the website?");
 
- setTimeout(sayhallo,1000);
+// const myConfirom = confirm( "Do you want to open the website?");
 
- function sayhallo(){
+//  setTimeout(sayhallo,1000);
+
+//  function sayhallo(){
     mybtn.onclick = function(){
       if(mybtn.className === "test"){
         const myImage = document.createElement("img");
@@ -41,7 +42,7 @@ const myConfirom = confirm( "Do you want to open the website?");
     
       }
 }
- }
+//   }
 
 mybtn.onblur = function (){
     myDiv.style.cssText = "display:none";
@@ -69,4 +70,85 @@ function countdown(){
 //     console.log("hallo Berlin ich bin----")
 // },2000);
 
-// --------------------------------------------------------------
+
+const myClick = document.getElementById("click");
+const myShow = document.getElementById("show");
+
+const myTime = setInterval( writmassege,2000);
+
+function writmassege(){
+    console.log("hallo Every 2 Secondary");
+}
+// myClick.onclick= function(){
+//     clearInterval(myTime);
+// }
+
+
+function writmassege(){
+
+    if(myShow.textContent <= 0){
+        myShow.textContent = "Done.... ";
+        clearInterval(myTime);
+
+    }
+else{
+   myShow.textContent = myShow.textContent -1;
+}
+
+
+}
+
+// ------------------------------offsetHeight/ width...... clientWidth / height.....---------------->
+// console.log(mybtn.offsetHeight);
+// console.log(mybtn.clientWidth);
+// dann können wir den Beide benuzten,um die höhe und bereite die Webwiesen....
+
+const myCite = document.getElementById("cite");
+
+
+console.log(myCite)
+onscroll = function(){
+    if(scrollY >= 200){
+      myCite.style.cssText = "display: block;background:green";
+    }else{
+       myCite.style.cssText = "display:none";
+    }
+
+}
+myCite.onclick = function(){
+    this.scroll({
+        top:0,
+        left:0,
+        behavior:"smooth",
+
+    })
+}
+
+// ---------- Window location Object------------>
+
+
+console.log(location);
+console.log(location.href);
+
+// location.href = "https://www.google.com/";
+//location.href = "/#number";
+// location.href = "https://developer.mozilla.org/en-US/";
+
+// console.log(location.host);
+// console.log(location.hostname)
+
+// console.log(location.protocol)
+
+//console.log(location.hash) // id meinte er hier
+console.log(location.reload)
+
+// ---------- Window Open And close------------>
+
+// setTimeout(function(){
+//     window.open("https://google.com","_blank","width=400,height=400,left=200,top=200")
+// },2000);
+
+
+// ---------- Window History object------------>
+console.log(history)
+
